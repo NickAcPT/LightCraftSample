2b8243109e4e319eaefcc4105cb8286e90a474ee
NickAcPT <32451103+NickAcPT@users.noreply.github.com>
2021-09-05 16:05:19 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+import io.github.nickacpt.lightcraft.gradle.minecraft.ClientVersion
+
+plugins {
+    java
+    id("io.github.nickacpt.lightcraft.gradle") version "1.0-SNAPSHOT"
+}
+
+group = "io.github.nickacpt.lightcraft.sample"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+lightcraft {
+    clientVersion = ClientVersion.V1_7_10
+    provideOptifineJarMod = true
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement {
+    repositories {
+        maven("https://raw.githubusercontent.com/NickAcPT/LightCraftMaven/main/")
+        mavenCentral()
+        maven("https://jitpack.io/")
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.quiltmc.org/repository/release/")
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "LightCraftSample"
+
